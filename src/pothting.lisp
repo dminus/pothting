f60b56e061963a9337ef1977f3205ea261c0fe4f
George Hamilton <george.hamilton@vetspire.com>
2026-03-10 21:25:25 -0500

    ah

--- src/pothting.lisp
+++ src/pothting.lisp
@@ -7,9 +7,30 @@
   "Server instance (Hunchentoot acceptor).")
 
 (defparameter *port* 8899 "The application port.")
+(defparameter *template-root* "
+<title>Pothting</title>
+  <body>
+    <h1>Welcome to Pothting</h1>
+    <hr>
+    <ul>
+      {% for article in articles %}
+        <li>
+        {{ article.1 }} - {{ article.2 }}
+        </li>
+      {% endfor %}
+    </ul>
+  </body>
+")
 
-(easy-routes:defroute root ("/") ()
-  "hello app")
+
+(defun render-articles ()
+  (djula:render-template* 
+    (djula:compile-string *template-root*)
+    nil
+    :articles (articles)))
+
+(easy-routes:defroute root ("/" :method :get) ()
+  (render-articles))
 
 (defun start-server (&key (port *port*))
   (format t "~&Starting the web server on port ~a~&" port)
